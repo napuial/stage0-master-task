7ad51ba1db385a162cd1b9c2e790f51d6448a8e8
napuial <125679202+napuial@users.noreply.github.com>
2024-07-10 19:14:02 +0200

    solutions

--- src/main/java/com/epam/loops/HalfPyramid.java
+++ src/main/java/com/epam/loops/HalfPyramid.java
@@ -3,7 +3,13 @@ package com.epam.loops;
 public class HalfPyramid {
 
     public void printHalfPyramid(int cathetusLength) {
-
+        for (int j = 1; j <= cathetusLength; j++) {
+            System.out.print(" ".repeat(cathetusLength - j));
+            for (int i = 0; i < j; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -3,7 +3,26 @@ package com.epam.loops;
 public class Pyramid {
 
     public void printPyramid(int cathetusLength) {
-
+        String[] result = new String[cathetusLength];
+        String temp = "";
+        int pyramidHeight = cathetusLength - 1;
+        int counter = 0;
+        for (int j = cathetusLength; j > 0 ; j--) {
+            for (int i = cathetusLength; i > 1; i--) {
+                temp += i;
+            }
+            for (int i = 1; i <= cathetusLength; i++) {
+                temp += i;
+            }
+            result[counter] = temp;
+            temp = "";
+            counter++;
+            cathetusLength--;
+        }
+        for (int i = pyramidHeight; i >= 0; i--) {
+            System.out.print(" ".repeat(i));
+            System.out.println(result[i]);
+        }
     }
 
     public static void main(String[] args) {
